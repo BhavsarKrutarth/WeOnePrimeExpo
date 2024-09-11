da5b4891e78e343ace8dff31611fa22f642ee1c5
NencySavaliya03 <166900744+NencySavaliya03@users.noreply.github.com>
2024-09-11 15:48:08 +0530

    amenities

--- weoneprime/Sources/navigation/Routes.js
+++ weoneprime/Sources/navigation/Routes.js
@@ -4,17 +4,19 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Index from "./BottomTabs";
 import AuthNavigation from "./AuthNavigation"; 
 import Amenities from "../screens/Main/HomeComponent/Amenities";
+import { useCustomFonts } from "../theme";
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
   const [isAuth, setAuth] = useState(false); 
-
+  useCustomFonts();
+  
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {isAuth ? (
-          <Stack.Screen name="Tab" component={Index} /> 
+          <Stack.Screen name="Tab" component={Amenities} /> 
         ) : (
           <Stack.Screen name="Auth">
             {(props) => <AuthNavigation {...props} setAuth={setAuth} />} 
--- weoneprime/Sources/screens/Main/HomeComponent/Amenities.js
+++ weoneprime/Sources/screens/Main/HomeComponent/Amenities.js
@@ -1,15 +1,18 @@
 import { FlatList, StyleSheet, Text, View, Image, TouchableOpacity, Pressable } from 'react-native';
-import React from 'react';
-import { RNImage, RNStyles, RNText } from '../../../common';
+import React, { useState } from 'react';
+import { RNImage, RNStyles } from '../../../common';
 import { Images } from '../../../constants';
-import { Colors, FontFamily, FontSize, hp, useCustomFonts, wp } from '../../../theme';
+import { Colors, FontFamily, FontSize, hp, wp } from '../../../theme';
 import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export default function Amenities() {
+  const [selectedHearts, setSelectedHearts] = useState([]); 
+
   const Data = [
     {
       id: 1,
-      image: Images.exploreimg,
+      image: require('../../../assets/images/amrutbanner.png'),
       brandLogo: Images.emp_logo,
       title: "Amrut",
       subtitle: "The fashion icon ",
@@ -18,55 +21,92 @@ export default function Amenities() {
       id: 2,
       image: Images.exploreimg,
       brandLogo: Images.emp_logo,
-      title: "Amrut",
-      subtitle: "The fashion icon ",
+      title: "Inox",
+      subtitle: "Live the movie",
     },
     {
       id: 3,
       image: Images.exploreimg,
       brandLogo: Images.emp_logo,
-      title: "Amrut",
-      subtitle: "The fashion icon ",
+      title: "Coffee Culture",
+      subtitle: "The taste of freshness",
     },
     {
       id: 4,
       image: Images.exploreimg,
       brandLogo: Images.emp_logo,
-      title: "Amrut",
-      subtitle: "The fashion icon ",
+      title: "Gollers",
+      subtitle: "Gollers locho khaman",
     },
     {
       id: 5,
       image: Images.exploreimg,
       brandLogo: Images.emp_logo,
-      title: "Amrut",
-      subtitle: "The fashion icon ",
+      title: "Coffee Culture",
+      subtitle: "The taste of freshness",
     },
     {
       id: 6,
       image: Images.exploreimg,
       brandLogo: Images.emp_logo,
-      title: "Amrut",
-      subtitle: "The fashion icon ",
-    },
+      title: "Gollers",
+      subtitle: "Gollers locho khaman",
+    }
   ];
 
-  const renderItem = ({ item }) => (
-    <Pressable style={styles.card}>
-      <Image source={item.image} style={styles.image} />
-      <LinearGradient
-        start={{ x: 0, y: .9 }}
-        end={{ x: 0, y: 0 }}
-        colors={['white', 'transparent']} 
-        style={styles.gradient}
-      />
-      <View style={styles.infoContainer}>
-        <RNImage source={item.brandLogo} style={{height: wp(10),width: wp(10)}} />
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.subtitle}>{item.subtitle}</Text>
-      </View>
-    </Pressable>
-  );
+  const handleHeartPress = (id) => {
+    setSelectedHearts(prevSelectedHearts => {
+      if (prevSelectedHearts.includes(id)) {
+        return prevSelectedHearts.filter(heartId => heartId !== id);
+      } else {
+        return [...prevSelectedHearts, id];
+      }
+    });
+  };
+
+  const renderItem = ({ item }) => {
+    const isSelected = selectedHearts.includes(item.id); 
+
+    return (
+      <Pressable style={styles.card}>
+        <Image source={item.image} style={styles.image} />
+        <LinearGradient
+          start={{ x: 0, y: 0.9 }}
+          end={{ x: 0, y: 0 }}
+          colors={['white', 'transparent']}
+          style={styles.gradient}
+        />
+        <TouchableOpacity
+          style={{
+            ...RNStyles.center,
+            height: wp(8),
+            width: wp(8),
+            backgroundColor: isSelected ? 'rgba(222, 33, 39, 0.9+)' : 'rgba(255, 255, 255, 0.35)',
+            position: 'absolute',
+            top: hp(1),
+            borderRadius: 50,
+            right: wp(2),
+          }}
+          onPress={() => handleHeartPress(item.id)} 
+        >
+          <Icon
+            name={isSelected ? "heart" : "heart"} 
+            solid={isSelected}
+            style={{
+              fontSize: FontSize.font14,
+              color: Colors.White,
+            }}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.infoContainer}>
+          <RNImage source={item.brandLogo} style={{ height: wp(10), width: wp(10) }} />
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+        </View>
+      </Pressable>
+    );
+  };
 
   return (
     <View style={styles.ExploreData}>
@@ -74,7 +114,7 @@ export default function Amenities() {
         data={Data}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
-        numColumns={2} 
+        numColumns={2}
         showsVerticalScrollIndicator={false}
       />
     </View>
@@ -86,11 +126,11 @@ const styles = StyleSheet.create({
     gap: hp(2),
     backgroundColor: Colors.White,
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   card: {
     backgroundColor: Colors.White,
-    borderRadius: wp(3),
+    borderRadius: wp(2),
     overflow: 'hidden',
     width: wp(45),
     height: wp(45),
@@ -106,25 +146,25 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    height: hp(25), 
+    height: hp(22),
   },
   infoContainer: {
     position: 'absolute',
-    bottom: hp(-.5),
+    bottom: hp(-0.5),
     left: wp(3),
     right: wp(2),
     zIndex: 100,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   title: {
-    fontSize: FontSize.font12,
+    fontSize: FontSize.font14,
     fontFamily: FontFamily.SemiBold,
-    color: Colors.Black, 
+    color: Colors.Black,
   },
   subtitle: {
-    fontSize: FontSize.font10,
-    fontFamily: FontFamily.Medium,
+    fontSize: FontSize.font11,
+    fontFamily: FontFamily.Light,
     marginBottom: hp(1),
-    color: Colors.Grey
+    color: Colors.Grey,
   },
 });
